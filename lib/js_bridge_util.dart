da4e7b2b6276206e54c3eec9764378180d32956e
Kuangsong <519089437@qq.com>
2022-03-21 11:49:08 +0800

    feat: init

---
+++ lib/js_bridge_util.dart
@@ -0,0 +1,34 @@
+library js_bridge_util;
+
+import 'dart:convert';
+
+class DsBridge {
+  final flutterWebViewPlugin;
+  String callBackCode = '';
+  DsBridge(this.flutterWebViewPlugin);
+
+  Result dispatch (String jsonStr) {
+    Map jsonData = jsonDecode(jsonStr);
+    String method = jsonData['method'];
+    String data = jsonData['data'];
+    callBackCode = jsonData['callBack'];
+    final result = Result();
+    result.method = method;
+    result.data = data;
+    result.callBack = callBack;
+    return result;
+  }
+
+  void callBack (String params) {
+    String code = '$callBackCode("$params")';
+    print('$code');
+    flutterWebViewPlugin.evalJavascript('$code');
+  }
+
+}
+
+class Result {
+  String method = '';
+  String data = '';
+  Function? callBack;
+}
